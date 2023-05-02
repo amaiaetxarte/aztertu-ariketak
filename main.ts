98e44e0b1787d844ed2e5cfef640d6b2cdfbdcf0
amaiaetxarte <132052285+amaiaetxarte@users.noreply.github.com>
2023-05-02 14:57:25 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+function soinua2 () {
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+}
+function soinua1 () {
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+}
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        soinua1()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        soinua2()
+    }
+})
